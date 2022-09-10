ce9af4df6b0683bc85beff67ee7881705bca7a57
unknown <jeo.anderson2@gmail.com>
2022-09-10 16:01:32 -0500

    Initial Commit

---
+++ lib/gtube.ex
@@ -0,0 +1,18 @@
+defmodule Gtube do
+  @moduledoc """
+  Documentation for `Gtube`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Gtube.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
