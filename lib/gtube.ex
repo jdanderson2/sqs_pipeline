a04fd2231967ea18d90da61bffcaae7c0211b5d7
unknown <jeo.anderson2@gmail.com>
2022-09-11 09:34:15 -0500

    edit files

--- lib/gtube.ex
+++ lib/gtube.ex
@@ -1,18 +1,18 @@
-defmodule Gtube do
-  @moduledoc """
-  Documentation for `Gtube`.
-  """
+defmodule GTube do
+  use Application
 
-  @doc """
-  Hello world.
+  def start (_Type, _args) do
+    import Supervisor.Spec
 
-  ## Examples
+    children = [
+      worker(SQS.Server, []),
+      worker(SQS.Producer, []),
+      supervisor(SQS.ConsumerSupervisor, ["Pipeline1"], id: 1),
+      supervisor(SQS.ConsumerSupervisor, ["Pipeline2"], id: 2),
+      supervisor(SQS.ConsumerSupervisor, ["Pipeline3"], id: 3),
+    ]
 
-      iex> Gtube.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    opts = [ strategy: one_for_one, name: ApplicationSupervisor]
+    Supervisor.start_link(children, opts)
   end
 end
