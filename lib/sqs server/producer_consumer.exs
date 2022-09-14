47cbb65aa4a2bc0e06db2cff2998915a6e2e382f
Jeorge D. Anderson II <jeo.anderson2@gmail.com>
2022-09-14 10:00:34 -0500

    Update producer_consumer.exs

--- lib/sqs server/producer_consumer.exs
+++ lib/sqs server/producer_consumer.exs
@@ -1,18 +1,32 @@
 defmodule Gtube.ProducerConsumber do
   use GenStage
 
-  require Integer
+# The Client API
 
-  def start_link(__initial) do
-    GenStage.start_link(__MODULE__, : state_doesnt_matter, name: __MODULE__)
-
+  def start_link(name_of_pipeline) do
+    process_name = Enum.join([name_of_pipeline, "ProducerConsumer"], "")
+    GenStage.start_link(__MODULE__, name_of_pipeline, name: String.to_atom(process_name))
   end
 
-  def handle_events(events, _from, state) do
-    numbers =
-    events
-    |>Enum.filter(&Integer.is_even/1)
+# The Server Callbacks
 
-    {:noreply, numbers, state}
+  def init(pipeline_name) do
+    {:producer_consumer, name_of_pipeline, subscribe_to: [{SQS.Producer, min_demand: 0, max_demand: 1}]}
+  end
+
+  def handle_events([events] = events, _from, name_of_pipeline) do
+    IO.puts "#{name_of_pipeline} ProducerConsumer received #{length events} events"
+    
+# Retrieve file from S3
+
+    {_status, %{body: zipped_file}} = ExAws.S3.get_object(event.bucket, event.key)
+    |> ExAws.request
+
+    file = :zlib.gunzip(zipped_file)
+
+    IO.puts "#{name_of_pipeline} ProducerConsumer downloaded #{event.key}"
+    
+# Pass the contents of that file to the consumer
+    {:noreply, [Map.put(event, :file, file)], name_of_pipeline}
   end
 end
